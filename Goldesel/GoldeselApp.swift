9e38108379217ed4f280648d33826629d189afde
schaeferri <rici.schaefer@gmail.com>
2023-03-15 22:38:51 +0100

    Copied default folders for iOS apps to goldesel

---
+++ Goldesel/GoldeselApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoldeselApp.swift
+//  Goldesel
+//
+//  Created by Rica Schäfer on 26.02.23.
+//
+
+import SwiftUI
+
+@main
+struct GoldeselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
